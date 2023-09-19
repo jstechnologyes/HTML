69880bf68c446eaccc0b0cde19b639cace85508d
Hadi1983 <hadi.mmpi@gmail.com>
2023-09-19 07:55:32 +0600

    Basic HTML Code

---
+++ me.js
@@ -0,0 +1,5 @@
+document.getElementById("text").innerHTML="Hello World";
+document.getElementById("text").style.color="blue";
+document.getElementById("text").style.fontSize="50px";
+document.getElementById("text").style.backgroundColor="red";
+document.getElementById("text").style.textAlign="center";
